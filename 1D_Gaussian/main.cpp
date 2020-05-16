d579e329b4c9099f439f03b57c0e2b18b5ad750d
milan-r-shah <mrshah@wpi.edu>
2020-05-15 20:08:22 -0400

    Created "1D_Gaussian" directory & added "main.cpp" file

---
+++ 1D_Gaussian/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+double f(double mu, double sigma2, double x)
+{
+    //Use mu, sigma2 (sigma squared), and x to code the 1-dimensional Gaussian
+    //Put your code here
+    //double prob =
+    return prob;
+}
+
+int main()
+{
+    cout << f(10.0, 4.0, 8.0) << endl;
+    return 0;
+}
