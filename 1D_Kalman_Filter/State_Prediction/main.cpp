43c9e046859a6c95da6e0d222fc5e128af8f28b9
Milan Shah <mrshah@wpi.edu>
2020-05-16 21:21:15 -0400

    Updated "state_prediction" func in "State_Prediction/main.cpp"

--- 1D_Kalman_Filter/State_Prediction/main.cpp
+++ 1D_Kalman_Filter/State_Prediction/main.cpp
@@ -8,8 +8,12 @@ double new_mean, new_var;
 
 tuple<double, double> state_prediction(double mean1, double var1, double mean2, double var2)
 {
-    new_mean = //TODO: Code the state prediction mean function mu;
-    new_var =  //TODO: Code the state prediction variance function sigma square;
+    // new_mean = //TODO: Code the state prediction mean function mu;
+    new_mean = mean1 + mean2;
+
+    // new_var =  //TODO: Code the state prediction variance function sigma square;
+    new_var = var1 + var2;
+    
     return make_tuple(new_mean, new_var);
 }
 
