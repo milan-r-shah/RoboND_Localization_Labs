de0806cb877b9f4b302e16b64340836faaf0d983
Milan Shah <mrshah@wpi.edu>
2020-05-18 18:27:59 -0400

    Updated main.cpp file in "Bayes_Fitering" dir

--- Bayes_Filtering/main.cpp
+++ Bayes_Filtering/main.cpp
@@ -2,18 +2,20 @@
 using namespace std;
 
 int main() {
-	
-	//Given P(POS), P(DOOR|POS) and P(DOOR|¬POS)
-	double a = 0.0002 ; //P(POS) = 0.002
-	double b = 0.6    ; //P(DOOR|POS) = 0.6
-	double c = 0.05   ; //P(DOOR|¬POS) = 0.05
-	
-	//TODO: Compute P(¬POS) and P(POS|DOOR)
-	double d =                   //P(¬POS)
-	double e =                   //P(POS|DOOR)
-	
-	//Print Result
-	cout << "P(POS|DOOR)= " <<    e    << endl;
-	
-	return 0;
+    // Given P(POS), P(DOOR|POS) and P(DOOR|¬POS)
+    double a = 0.0002;  //P(POS) = 0.002
+    double b = 0.6;     //P(DOOR|POS) = 0.6
+    double c = 0.05;    //P(DOOR|¬POS) = 0.05
+
+    // Compute P(¬POS) and P(POS|DOOR)
+    // P(A/B) = (P(B/A) * P(A)) / P(B)
+    //        = (P(B/A) * P(A)) / [ P(A) * P(B/A) + P(¬A) * P(B/¬A) ]
+
+    double d = 1 - a;                      //P(¬POS)
+    double e = (b * a) / (a * b + d * c);  //P(POS|DOOR)
+
+    // Print Result
+    cout << "P(POS|DOOR)= " << e << endl;
+
+    return 0;
 }
