dcbe051b5dddb3d00e10891cf27c5368b0c5c347
Milan Shah <mrshah@wpi.edu>
2020-05-20 18:16:39 -0400

    Updated main.cpp & added solution.cpp in "MCL/07_Resampling" dir

--- MCL/07_Resampling/main.cpp
+++ MCL/07_Resampling/main.cpp
@@ -6,11 +6,16 @@ double w[] = { 0.6, 1.2, 2.4, 0.6, 1.2 };//You can also change this to a vector
 
 //TODO: Define a  ComputeProb function and compute the Probabilities
 
+void ComputeProb() {
+    double totalW = 0.0;
+    for(int i = 0; i < sizeof(w)/sizeof(w[0]); ++i) {
+        totalW += w[i];
+    }
 
-
-
-
-
+    for(int i = 0; i < sizeof(w)/sizeof(w[0]); ++i) {
+        cout << "P"<< i+1 << "=" <<  w[i] / totalW << endl;
+    }
+}
 
 int main()
 {
@@ -19,10 +24,7 @@ int main()
     //:
     //P5=Value
     
-    
-    
-    
-    
+    ComputeProb();
     
     return 0;
 }
