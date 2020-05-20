1de9476b6091b43bcb499849b31f517bcb1c0bd7
Milan Shah <mrshah@wpi.edu>
2020-05-19 22:14:12 -0400

    Created "07_Resampling" dir in "MCL" dir and added main.cpp file

---
+++ MCL/07_Resampling/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+double w[] = { 0.6, 1.2, 2.4, 0.6, 1.2 };//You can also change this to a vector
+
+//TODO: Define a  ComputeProb function and compute the Probabilities
+
+
+
+
+
+
+
+int main()
+{
+    //TODO: Print Probabilites each on a single line:
+    //P1=Value
+    //:
+    //P5=Value
+    
+    
+    
+    
+    
+    
+    return 0;
+}
